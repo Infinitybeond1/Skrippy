854e792c0dcc34ea310b3e1c46426bb3048dadfb
Infinitybeond1 <Infinitybeond1@protonmail.com>
2022-03-10 22:43:32 -0600

    commands dir

--- handler/index.js
+++ handler/index.js
@@ -15,7 +15,7 @@ module.exports = async (client) => {
 
     // Slash Commands
     const slashCommands = await globPromise(
-        `${process.cwd()}/SlashCommands/*/*.js`
+        `${process.cwd()}/Commands/*/*.js`
     );
 
     const arrayOfSlashCommands = [];
